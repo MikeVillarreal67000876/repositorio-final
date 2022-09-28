d17ec2865e8a3a0573c26f00a331ddc70a3e50b4
MikeVillarreal67000876 <Mavillarreal76@ucatolica.edu.co>
2022-09-28 12:12:31 -0500

    Primer commit

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Estudiante.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author sala4
+ */
+public class Estudiante extends Usuario {
+private int codigo;
+private String correoinstitucional;
+
+    public Estudiante(int codigo, String correoinstitucional, String email, String nombres, String apellido, int CC) {
+        super(email, nombres, apellido, CC);
+        this.codigo = codigo;
+        this.correoinstitucional = CreadorCorreoInstitucional ();
+    }
+    public String CreadorCorreoInstitucional(){
+        return "";
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCorreoinstitucional() {
+        return correoinstitucional;
+    }
+
+    public void setCorreoinstitucional(String correoinstitucional) {
+        this.correoinstitucional = correoinstitucional;
+    }
+
+    
+}
