023de17fa0ac0c8a0d71104fb5c11de3a95f97ae
MikeVillarreal67000876 <Mavillarreal76@ucatolica.edu.co>
2022-09-28 23:48:45 -0500

    Version2

---
+++ Test/src/main/java/com/mycompany/test/NewClass.java
@@ -0,0 +1,18 @@
+package com.mycompany.test;
+
+
+import com.mycompany.test.Avestruz;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class NewClass {
+    Avestruz avestruz1 = new Avestruz() ;
+}
